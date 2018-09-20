d218a466dca64f724da2d346503d548ccf5926a5
godzyken <isgodzy@msn.com>
2018-09-20 10:50:33 +0200

    gérez la navigation
    Signed-off-by: godzy <isgodzy@msn.com>

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
 import { Injectable } from "@angular/core";
 
